496fe58e296d093cd7be709d4595e73e359b8153
liuyi20 <liuyi20@hikvision.com.cn>
2021-07-12 01:42:15 +0800

    modify yyhbxx to envPublic

--- js/index.js
+++ js/index.js
@@ -1,17 +1,63 @@
 /**
  * Created by MacOS on 2016/10/18.
  */
+
+var webSite = 'https://envpublic.github.io/';
+var webTitle = '企业环保信息公示平台';
+var phoneNum = '130-3299-0177';
+var BS = [
+    {
+        bsId: 1, 
+        show: true,
+        showDeadline: '2021-07-30',
+        bsName: '榆林双威环保建材有限公司',
+        publicDate: '2021 年 05 月 22 日', 
+        dropdownName: '榆林双威环保建材公司', 
+        download: '榆林双威环保建材有限公司.zip' 
+    },
+    { 
+        bsId: 2, 
+        show: true,
+        showDeadline: '2021-08-14',
+        bsName: '延安恒利达洗选煤有限责任公司', 
+        publicDate: '2021 年 06月 14 日', 
+        dropdownName: '恒利达洗选煤有限责任公司', 
+        download: '延安恒利达洗选煤有限责任公司.zip'
+     }
+
+]; 
+
 /* 入口函数 */
 (function () {
+    setHeader();
+    setFooter();
     getToday();
+
+    setDropdownContent();
     dropdown();
+
+    setBsContent();
     changeShanghu();
 })();
 
+function setHeader() {
+  $("#site_title").empty();
+  $("#site_title").html('<h1 class="h1Defined"><a href="'+ webSite +'" target="_parent">'+ webTitle +'<span>&nbsp;缔造健康的生活，维护和谐的自然环境</span></a></h1>');
+}
+
+/* 
+ 设置每个页面的底部内容
+ */
+function setFooter() {
+    $('#templatemo_footer').empty()
+    $('#templatemo_footer').html('<div class="section_w860"> Copyright © <span id="thisYear"></span> <a href="'+ webSite +'">'+ webTitle +'</a> | 平台公示请联系：<a>'+ phoneNum +'</a></div>')
+}
+
 /*获取并显示今日时间*/
 function getToday() {
-    var enabled = 0; today = new Date();
-    var day; var dateToday;
+    var today = new Date();
+    var day; 
+    var dateToday;
     if(today.getDay()==0) day = " 星期日";
     if(today.getDay()==1) day = " 星期一";
     if(today.getDay()==2) day = " 星期二";
@@ -21,11 +67,33 @@ function getToday() {
     if(today.getDay()==6) day = " 星期六";
     dateToday = (today.getFullYear()) + "年" + (today.getMonth() + 1 ) + "月" + today.getDate() + "日" + day +"";
     $("#dateToday").html(dateToday);
+    $("#thisYear").html(today.getFullYear());
 }
+
+function setDropdownContent () {
+    $('#dropdownMenu').empty();
+    var dropdownCon = '';
+    for (var i = 0; i < BS.length; i++) {
+        var item = BS[i];
+        var itemLi = '<li id="dropDown'+ item.bsId +'"><a href="javascript:void(0)">'+ item.dropdownName +'</a></li>';
+        dropdownCon += itemLi;
+    }
+    $('#dropdownMenu').html(dropdownCon)
+}
+
+function setBsContent () {
+    $("#BS_List").empty();
+    var BsCon = "";
+    for (var i = 0; i < BS.length; i++) {
+        var bsItem = BS[i];
+        BsCon += '<div id="bsList'+ bsItem.bsId +'" class="shanghu"><p class="title"><span>企业名称：'+ bsItem.bsName +'</span> <span>公示日期：'+ bsItem.publicDate +'</span></p><p style="margin:10px 20px;font-size:14px;color:#555;line-height:28px;"><strong>'+ bsItem.bsName +'</strong> 目前正在进行项目环境检测报告验收，验收监测工作当地环保局承担。现进行项目验收监测报告信息公示。</p><p><a style="margin-right:10px;margin-left:400px;" href="../database/'+ bsItem.download +'" target="_blank">点击下载 <span>'+ bsItem.bsName +'</span> 公示文件</a></p></div>';
+    }
+    $("#BS_List").html(BsCon);
+}
+
 
 // 添加商户在下来菜单
 function dropdown() {
-   
 $('#nav_menu>ul>li').hover(
     function() {
       /*  if($('#dropdownMenu').css('display')=="none"){
@@ -44,142 +112,20 @@ $("#clickHideMenu").off('click');
 $("#clickHideMenu").on('click',function(){
 	if($('#dropdownMenu').css('display')==""){
            $('#dropdownMenu').hide();
-           }
-	}
-);
- 
-    
+        }
+	});
 }
 
 //点击不同商户，切换相应的内容
 function changeShanghu(){
-    
-    $("#shuangWei").on('click',function(){
-       $("#shanghu41").show();
-       $("#shanghu41").siblings("div.shanghu").hide();
+    $("#dropDown1").on('click',function(){
+        $("#bsList1").show();
+        $("#bsList1").siblings("div.shanghu").hide();
     });
-    
-     $("#hengLiDa").on('click',function(){
-       $("#shanghu40").show();
-       $("#shanghu40").siblings("div.shanghu").hide();
+   
+    $("#dropDown2").on('click',function(){
+        $("#bsList2").show();
+        $("#bsList2").siblings("div.shanghu").hide();
     });
-    
-    $("#baoHeng").on('click',function(){
-       $("#shanghu39").show();
-       $("#shanghu39").siblings("div.shanghu").hide();
-    });
-    
-    $("#ruiQiang").on('click',function(){
-       $("#shanghu38").show();
-       $("#shanghu38").siblings("div.shanghu").hide();
-    });
-    
-    $("#houYuanXin").on('click',function(){
-       $("#shanghu37").show();
-       $("#shanghu37").siblings("div.shanghu").hide();
-    });
-    
-    $("#sanLian_1").on('click',function(){
-       $("#shanghu36").show();
-       $("#shanghu36").siblings("div.shanghu").hide();
-    });
-    
-    $("#ruYu").on('click',function(){
-       $("#shanghu35").show();
-       $("#shanghu35").siblings("div.shanghu").hide();
-    });
-    
-    $("#dongXinLai").on('click',function(){
-       $("#shanghu34").show();
-       $("#shanghu34").siblings("div.shanghu").hide();
-    });
-    
-    $("#heRun").on('click',function(){
-       $("#shanghu33").show();
-       $("#shanghu33").siblings("div.shanghu").hide();
-    });
-    
-    $("#luWen").on('click',function(){
-        $("#shanghu32").show();
-        $("#shanghu32").siblings("div.shanghu").hide();
-    });
-    
-    $("#feiHua").on('click',function(){
-        $("#shanghu31").show();
-        $("#shanghu31").siblings("div.shanghu").hide();
-    });
-    
-    $("#feiMaLiang").on('click',function(){
-        $("#shanghu30").show();
-        $("#shanghu30").siblings("div.shanghu").hide();
-    });
-    
-    $("#xinJuYuan").on('click',function(){
-        $("#shanghu29").show();
-        $("#shanghu29").siblings("div.shanghu").hide();
-    });
-    
-    $("#tianSui").on('click',function(){
-        $("#shanghu28").show();
-        $("#shanghu28").siblings("div.shanghu").hide();
-    });
-    
-     $("#yongHong").on('click',function(){
-        $("#shanghu27").show();
-        $("#shanghu27").siblings("div.shanghu").hide();
-    });
-    
-     $("#jinJiang").on('click',function(){
-        $("#shanghu26").show();
-        $("#shanghu26").siblings("div.shanghu").hide();
-    });
-    
     
 }
-
-/*  ShangHu Lists
-
- 1.2.3.... ...
- 
- 21.22...
- 
- 23. 府谷县银海源煤焦有限公司   2016/11/23 [2016-12-02]
-
- 24. 府谷县大昌汗昌源热力有限公司  Modify :( 2017/05/08 ) -- Show :[ 2017/05/08 ]
- 
- 25. 府谷县麟瑞煤焦运销有限责任公司  Modify :( 2017/06/05 ) -- Show :[ 2017/05/19 ]
- 
- 26. 府谷县金江煤业有限公司  Modify :( 2017/07/17 ) -- Show :[ 2017/07/13 ]
- 
- 27. 府谷县永宏煤炭有限责任公司  Modify :( 2017/07/31 ) -- Show :[ 2017/07/28 ]
- 
- 28. 府谷县天燧洗选煤业有限公司  Modify :( 2017/08/08 ) -- Show :[ 2017/07/14 ]
- 
- 29. 府谷县鑫聚源煤炭有限责任公司  Modify :( 2017/08/16 ) -- Show :[ 2017/08/16 ]
- 
- 30. 府谷县飞马梁洗选煤有限公司  Modify :( 2017/09/14 ) -- Show :[ 2017/09/06 ]
- 
- 31. 府谷县飞华煤炭有限责任公司  Modify :( 2017/09/25 ) -- Show :[ 2017/09/01 ]
- 
- 32. 府谷县鲁文环保建材有限责任公司  Modify :( 2017/09/25 ) -- Show :[ 2017/09/05 ]
- 
- 33. 府谷县和润煤炭有限责任公司  Modify :( 2017/09/25 ) -- Show :[ 2017/09/06 ]
- 
- 34. 府谷县东鑫莱煤化工有限责任公司  Modify :( 2017/09/28 ) -- Show :[ 2017/09/28 ]
- 
- 35. 陕西普宇集团如雨实业有限公司  Modify :( 2017/09/30 ) -- Show :[ 2017/09/29 ]
-  
- 36. 府谷县三联煤电化有限责任公司第一分公司  Modify :( 2017/09/30 ) -- Show :[ 2017/09/29 ]
- 
- 37. 府谷县厚源型煤有限责任公司  Modify :( 2017/09/30 ) -- Show :[ 2017/09/29 ]
-  
- 38. 府谷县瑞强工贸有限责任公司  Modify :( 2017/09/30 ) -- Show :[ 2017/09/29 ]
-   
- 39. 府谷县宝恒商贸有限公司  Modify :( 2017/09/30 ) -- Show :[ 2017/09/30 ]
- 
- 40. 府谷县恒利达洗选煤有限责任公司  Modify :( 2017/10/24 ) -- Show :[ 2017/09/30 ]
-    
- 41. 府谷县双威环保建材有限公司  Modify :( 2017/10/25 ) -- Show :[ 2017/09/30 ]
-
-*/
-
